8f9b508f1ceef19575a44a2d8ca81b17b5ee54ab
KlnSdr <monsieurk1209@gmail.com>
2023-10-14 13:48:39 +0200

    change base path for buckets

--- src/thot/Main.java
+++ src/thot/Main.java
@@ -4,7 +4,7 @@ import thot.buckets.service.BucketService;
 import thot.server.Server;
 
 public class Main {
-    public static final String basePath = "thot/buckets/";
+    public static final String basePath = "buckets/";
 
     public static void main(String[] args) {
         Server.startApplication();
