626e1e7272979b3e9feba2a894fc16b414a38261
KlnSdr <monsieurk1209@gmail.com>
2023-12-19 20:48:44 +0100

    bump dobby version to v0.0.3

--- src/thot/Main.java
+++ src/thot/Main.java
@@ -1,13 +1,23 @@
 package thot;
 
 import dobby.Dobby;
+import dobby.DobbyEntryPoint;
 import thot.buckets.service.BucketService;
 
-public class Main {
+public class Main implements DobbyEntryPoint {
     public static final String basePath = "buckets/";
 
     public static void main(String[] args) {
-        BucketService.getInstance();
         Dobby.startApplication(Main.class);
+    }
+
+    @Override
+    public void preStart() {
+        BucketService.getInstance();
+    }
+
+    @Override
+    public void postStart() {
+
     }
 }
