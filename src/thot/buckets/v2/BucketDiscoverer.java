3bae1aabcdcc2c1c60760e3b0db58f07353ae7bf
KlnSdr <monsieurk1209@gmail.com>
2024-08-09 21:12:28 +0200

    add annotation vor Bucket v2 + copy impl of BucketDiscoverer to v2

--- src/thot/buckets/v2/BucketDiscoverer.java
+++ src/thot/buckets/v2/BucketDiscoverer.java
@@ -1,4 +1,50 @@
 package thot.buckets.v2;
 
-public class BucketDiscoverer {
+import dobby.util.Classloader;
+import dobby.util.logging.Logger;
+import thot.buckets.Bucket;
+import thot.buckets.service.BucketService;
+
+public class BucketDiscoverer extends Classloader<Object> {
+    private static final Logger LOGGER = new Logger(thot.buckets.BucketDiscoverer.class);
+
+    private BucketDiscoverer(String packageName) {
+        this.packageName = packageName;
+    }
+
+    /**
+     * Discovers routes in a given package
+     *
+     * @param rootPackage Root package
+     */
+    public static void discoverRoutes(String rootPackage) {
+        if (rootPackage.startsWith(".")) {
+            rootPackage = rootPackage.substring(1);
+        }
+        BucketDiscoverer discoverer = new BucketDiscoverer(rootPackage);
+        discoverer.loadClasses().forEach(discoverer::analyzeClass);
+        String finalRootPackage = rootPackage;
+        discoverer.getPackages().forEach(subpackage -> thot.buckets.BucketDiscoverer.discoverRoutes(finalRootPackage + "." + subpackage));
+    }
+
+    private void analyzeClass(Class<?> clazz) {
+        if (clazz.isAnnotationPresent(thot.annotations.v2.Bucket.class)) {
+            final String bucketName = clazz.getAnnotation(thot.annotations.Bucket.class).value();
+
+            Bucket bucket = BucketService.getInstance().find(bucketName);
+
+            if (bucket == null) {
+                LOGGER.info("Could not find bucket '" + bucketName + "'. Creating it now");
+                bucket = BucketService.getInstance().create(bucketName);
+
+                bucket.write("test", "test_value");
+                bucket.delete("test");
+            }
+        }
+    }
+
+    @Override
+    protected Class<?> filterClasses(String s) {
+        return defaultClassFilter(s);
+    }
 }
