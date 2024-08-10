0efea0f3d8ab7cf4ec29be4403e2e1ea00df9be0
KlnSdr <monsieurk1209@gmail.com>
2024-08-10 15:30:04 +0200

    dont send sub buckets when requesting all buckets + sort bucket names

--- src/thot/server/handler/names/BucketNameHandler.java
+++ src/thot/server/handler/names/BucketNameHandler.java
@@ -6,6 +6,7 @@ import thot.common.response.Response;
 import thot.common.response.ResponseType;
 import thot.server.handler.Handler;
 
+import java.util.Arrays;
 import java.util.Set;
 
 public class BucketNameHandler implements Handler {
@@ -16,7 +17,10 @@ public class BucketNameHandler implements Handler {
         final Response response = new Response();
         response.setResponseType(ResponseType.SUCCESS);
         final Set<String> bucketNames = bucketService.getBucketNames();
-        response.setValue(bucketNames.toArray(new String[0]));
+        bucketNames.stream().filter(bucketName -> bucketName.contains("-")).forEach(bucketNames::remove); // filter out all sub-buckets
+        final String[] bucketNamesArray = bucketNames.toArray(new String[0]);
+        Arrays.sort(bucketNamesArray);
+        response.setValue(bucketNamesArray);
 
         return response;
     }
